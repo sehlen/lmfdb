471e3961a4673a55829c802c93326f4c8c538c54
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2013-01-24 15:57:49 +0000

    Fixed to line that checks if the Modulae has a precomputed decomposition.

--- lmfdb/modular_forms/elliptic_modular_forms/test/test_unpickle.py
+++ lmfdb/modular_forms/elliptic_modular_forms/test/test_unpickle.py
@@ -1,8 +1,7 @@
-import pymongo
+import pymongo, gridfs
+from sage.all import loads
 dbport = 37010
 C = pymongo.connection.Connection(port=int(37010))
-
-
 
 def test_unpickle():
     r"""
@@ -13,21 +12,24 @@ def test_unpickle():
     """
     modforms=C['modularforms']
     modular_symbols = modforms['Modular_symbols']
-    files = C[db_name][collection].files
+    files = modular_symbols.files
     finds = files.find()
     n=finds.count()
+    print n, ' records found.'
     i=1
     for rec in finds:
         fid = rec['_id']
-        fs = gridfs.GridFS(C[db_name], collection)
+        fs = gridfs.GridFS(C['modularforms'], 'Modular_symbols')
         f = fs.get(fid)
         M = loads(f.read())
-        S = M.cuspidal_submodule().decomposition()[0].free_module().basis()
-        try:
-            print hash(S)
-            print 'Successfully tested hash ' + i + '/' + n
-            i=i+1
-        except:
-            'Hashing failed for' + M, S
-            return M
+        A = M.ambient_hecke_module()
+        if hasattr(A,"_HeckeModule_free_module__decomposition"):
+            S = M.cuspidal_submodule().decomposition()[0].free_module().basis()
+            try:
+                print hash(S)
+                print 'Successfully tested hash ', i , '/', n
+                i=i+1
+            except:
+                'Hashing failed for' + M, S
+                return M
             
