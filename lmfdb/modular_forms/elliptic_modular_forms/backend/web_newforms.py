d5e0920de83bffced28b12820f19333aa0a6a049
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-05-18 18:11:55 -0400

    Make hiding of q-expansions more informative and work in all cases. Still have to tune the default value for default_max_height, wich controls when to hide a Fourier expansion. We now also display the trace and the approximate norm of the "first" nonvanishing Fourier coefficient.
    This fixes https://github.com/LMFDB/lmfdb/issues/1385
    although there is certainly room for improvement.

--- lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
+++ lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
@@ -400,6 +400,27 @@ class WebNewForm(WebObject, CachedRepresentation):
             c = self.coefficients([n])[0] 
         return c
 
+    def first_nonvanishing_coefficient(self, return_index = True):
+        r"""
+         Return the first Fourier coefficient of self
+         of index >1 that does not vanish.
+         if return_index is True, we also return the index of that coefficient
+        """
+        for n in range(2,self.prec):
+            if self.coefficient(n) != 0:
+                if return_index:
+                    return n, self.coefficient(n)
+                else:
+                    return self.coefficient(n)
+
+    def complexity_of_first_nonvanishing_coefficient(self):
+        c = self.first_nonvanishing_coefficient(return_index=False)
+        l = c.list()
+        if l[0].parent().absolute_degree() == 1:
+            return len(str(max(r.height() for r in l)))*len(l)
+        else:
+            return len(str(max(r.height()*len(s.list()) for s in l for r in s.list())))*len(l)
+
     def coefficient_embedding(self,n,i):
         r"""
         Return the i-th complex embedding of coefficient C(n).
